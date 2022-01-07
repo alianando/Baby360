9c3e697304e8d3bf3e99d6046f274b1427cec080
ali anando <alianando44@gmail.com>
2022-01-08 02:22:54 +0600

    firebase image

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIO6SmTFi6YKUV4xRJxsNF_whCKxqEIVQ",
+  authDomain: "baby360-f3e1b.firebaseapp.com",
+  projectId: "baby360-f3e1b",
+  storageBucket: "baby360-f3e1b.appspot.com",
+  messagingSenderId: "379769803501",
+  appId: "1:379769803501:web:f7a3a1e39bd6a82319b2e4",
+  measurementId: "G-BXLY4M486N",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const storage = getStorage(firebaseApp);
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,61 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./navbar.css";
 import { useHistory } from "react-router";
+import { storage } from "../../Config";
+import { ref, getDownloadURL } from "firebase/storage";
 
 export default function NavBar() {
+  const [files, setFiles] = useState("");
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      // let result = await storage.ref().child("").listAll();
+      // let urlPromises = result.items.map((imageRef) =>
+      //   imageRef.getDownloadURL()
+      // );
+
+      // return Promise.all(urlPromises);
+      const starsRef = await ref(storage, "Baby360-logo1.png");
+      // Get the download URL
+      await getDownloadURL(starsRef)
+        .then((url) => {
+          console.log(url);
+          setFiles(url);
+        })
+        .catch((error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/object-not-found":
+              // File doesn't exist
+              break;
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect the server response
+              break;
+          }
+        });
+    };
+    fetchImages();
+
+    // const loadImages = async () => {
+    //   const urls = await fetchImages();
+    //   setFiles(urls);
+    // };
+    // loadImages();
+  }, []);
+
+  // console.log(files);
+
   const history = useHistory();
   return (
     <div className="Nav-Grid">
@@ -15,7 +68,8 @@ export default function NavBar() {
           });
         }}
       >
-        <img src={"/images/logo/Baby360-logo1.png"} alt="logo" />
+        {/* <img src={"/images/logo/Baby360-logo1.png"} alt="logo" /> */}
+        <img src={files} alt="logo" />
       </div>
       <div className="Nav-Search">
         <input type="search" placeholder="SEARCH" />
@@ -27,7 +81,8 @@ export default function NavBar() {
       <div className="Nav-Menu">
         <div>
           <div className="itemsNav">
-            <div className="button"
+            <div
+              className="button"
               onClick={() => {
                 history.push({
                   pathname: "/products",
@@ -35,7 +90,7 @@ export default function NavBar() {
               }}
             >
               <img src={"/images/Catagory/store.svg"} alt="store" />
-              <div className="">Store</div>
+              <div className="">{files}</div>
             </div>
             {/* <li>
             <img src={"/images/Catagory/store.svg"} alt="logo" />
